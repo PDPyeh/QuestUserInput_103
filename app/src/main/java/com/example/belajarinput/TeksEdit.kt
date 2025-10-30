95a1792a08a10ce8394eb7f830f33599a4f9c465
Pradipa Y <merakayamsstraat@gmail.com>
2025-10-30 14:57:34 +0700

    membuat var textNama di function formDataDiri di TeksEdit.kt

--- app/src/main/java/com/example/belajarinput/TeksEdit.kt
+++ app/src/main/java/com/example/belajarinput/TeksEdit.kt
@@ -1,10 +1,12 @@
 package com.example.belajarinput
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import java.lang.reflect.Modifier
 
 @Composable
 fun FormDataDiri(modifier: Modifier)
 {
-
+    var textNama by remember { mutableStateOf(value = "") }
 }
